5ca8c3c03fb65bf3de1a425ff8aab122c06b0b09
SAYEED AJMAL <sayeedajmala06@gmail.com>
2024-08-05 21:37:08 +0530

    fixed Some FeignErroDecoder

--- PostService/src/main/java/com/strong/PostService/Utils/FeignErrorDecoder.java
+++ PostService/src/main/java/com/strong/PostService/Utils/FeignErrorDecoder.java
@@ -1,6 +1,12 @@
 package com.strong.PostService.Utils;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import feign.Response;
 import feign.codec.ErrorDecoder;
@@ -8,11 +14,26 @@ import feign.codec.ErrorDecoder;
 @Component
 public class FeignErrorDecoder implements ErrorDecoder {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public Exception decode(String methodKey, Response response) {
-        if (response.status() == 409) {
-            return new BlogException("Author Not Found by this Id: ");
+        try {
+            if (response.body() != null) {
+                String responseBody = new String(response.body().asInputStream().readAllBytes(),
+                        StandardCharsets.UTF_8);
+
+                JsonNode errorResponse = objectMapper.readTree(responseBody);
+
+                String message = errorResponse.path("message").asText("No message available");
+
+                return new BlogException(
+                        String.format("%s", message));
+            }
+        } catch (IOException e) {
+            return new BlogException("Failed to read or parse response body", e);
         }
-        return new Default().decode(methodKey, response);
+
+        return new BlogException("Unknown error occurred");
     }
 }

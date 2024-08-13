164cc1928ad270e1e8e8263e79f2a5e2365a9df1
SAYEED AJMAL <sayeedajmala06@gmail.com>
2024-08-13 17:14:36 +0530

    AuthorService - added Service Discovery

--- AuthorService/src/main/java/com/strong/AuthorService/AuthorServiceApplication.java
+++ AuthorService/src/main/java/com/strong/AuthorService/AuthorServiceApplication.java
@@ -3,8 +3,10 @@ package com.strong.AuthorService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 public class AuthorServiceApplication {
 
 	public static void main(String[] args) {

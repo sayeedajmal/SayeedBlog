ea22ac880ae08defb9d5b63ef597341cf9f810d2
SAYEED AJMAL <sayeedajmala06@gmail.com>
2024-08-25 13:06:49 +0530

    Gateway - Added

---
+++ blogGateway/src/main/java/com/strong/blogGateway/BlogGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.strong.blogGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogGatewayApplication.class, args);
+	}
+
+}
---
+++ blogGateway/src/test/java/com/strong/blogGateway/BlogGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strong.blogGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
